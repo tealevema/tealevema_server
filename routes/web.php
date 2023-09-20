66f16e8086951446e02ddf729f9d0f5dbcae4551
tealevema <ryotaro20322.hit@gmail.com>
2023-09-20 14:51:27 +0900

    ブログ投稿詳細の表示

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
